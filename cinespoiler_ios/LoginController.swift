de1bc7f83b7e33ecef6f0f00586cc8f3c1f7fc7d
Hearly98 <shuertas981@gmail.com>
2024-11-30 19:13:50 -0500

    login y register controller

---
+++ cinespoiler_ios/LoginController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginController.swift
+//  cinespoiler_ios
+//
+//  Created by DAMII on 30/11/24.
+//
+
+import Cocoa
+
+class LoginController: UIViewController {
+
+}
---
+++ cinespoiler_ios/RegisterController.swift
@@ -0,0 +1,12 @@
+//
+//  RegisterController.swift
+//  cinespoiler_ios
+//
+//  Created by DAMII on 30/11/24.
+//
+
+import Cocoa
+
+class RegisterController: UIViewController {
+
+}
