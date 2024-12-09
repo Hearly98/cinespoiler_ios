229e5867889522d3c6a4a9004528f9c513f22aac
Hearly98 <shuertas981@gmail.com>
2024-12-09 12:54:01 -0800

    login con firebase

--- cinespoiler_ios/LoginController.swift
+++ cinespoiler_ios/LoginController.swift
@@ -5,31 +5,58 @@
 //  Created by DAMII on 30/11/24.
 //
 
-import Cocoa
+import UIKit
 import FirebaseAuth
 
 class LoginController:UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getUser()
+    }
+    
     @IBAction func loginBtn(_ sender: UIButton) {
-        guard let email = emailTextField.text,let password = passwordTextField.text
-                else print("Contraseña incorrecta")
-                    return
+        loginUser()
+        }
+    
+    func goToPush(){
+        let storyBoard = UIStoryboard.init(name: "Main", bundle: Bundle.main)
+        let viewController = storyBoard.instantiateViewController(withIdentifier: "") as? HomeViewController
+        viewController?.modalPresentationStyle = .overFullScreen
+        self.present(viewController ?? ViewController(), animated: true, completion: nil)
+    }
+    
+    func getUser(){
+        let _ = Auth.auth().addStateDidChangeListener{auth, user in
+            if user == nil {
+                print("error al logearse")
+            }else {
+                self.goToPush()
+            }}
+    }
+    func configureAlert(){
+        let alertController = UIAlertController(title: "Mensaje de error", message: "Email o contraseña incorrectos", preferredStyle: .alert)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    func loginWithFirebase(email: String, password: String){
+        Auth.auth().signIn(withEmail: email, password: password){
+            [weak self] authResult, error in
+            guard let self = self else {return}
+            if error != nil {
+                self.configureAlert()
+            }else {
+                self.goToPush()
+            }
+        }
     }
     
     
-    }
-
-//(\)
-
-Auth.auth().signIn(withEmail: email, password: password){
-    AuthDataResult, error in
-    if(let error = error){
-        print("Error al iniciar sesion: \(error.localizedDescription)")
+    func loginUser(){
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        loginWithFirebase(email: email, password: password)
     }
-    
-    print(AuthDataResult, "data result")
+    //(\)
 }
-
--- cinespoiler_ios/RegisterController.swift
+++ cinespoiler_ios/RegisterController.swift
@@ -5,8 +5,33 @@
 //  Created by DAMII on 30/11/24.
 //
 
-import Cocoa
+import UIKit
+import FirebaseAuth
 
 class RegisterController: UIViewController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func goToPush(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func configureAlert(){
+        let alertController = UIAlertController(title: "Mensaje de error", message: "Email o contraseña incorrectos", preferredStyle: .alert)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    func registerWithFirebase(email: String, password: String){
+        Auth.auth().createUser(withEmail: email, password: password){
+            [weak self] authResult, error in
+            guard let self = self else {return}
+            if error != nil {
+                self.configureAlert()
+            }else{
+                self.goToPush()
+            }
+        }
+    }
 }
